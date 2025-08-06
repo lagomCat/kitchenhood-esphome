cb627a71222a598b754a31f6c119c581ace22fc4
lagomCat <casiopeia80@gmail.com>
2025-08-07 02:40:43 +0500

    Удаление инструмента вставки паузы заданной продолжительности между фреймами UART для отладки.

--- custom_components/kitchen_hood/kitchen_hood.cpp
+++ custom_components/kitchen_hood/kitchen_hood.cpp
@@ -7,95 +7,42 @@ KitchenHood *KitchenHood::instance = nullptr;
 
 KitchenHood::KitchenHood(uart::UARTComponent *parent) : uart::UARTDevice(parent) {
   state_ = BOOT_SEND;
-  last_ts_ms_ = 0;
-  next_send_ts_ms_ = 0;
   instance = this;
 }
 
-void KitchenHood::setup() {
-  
-}
+void KitchenHood::setup() {}
 
-void KitchenHood::start_sequence(const std::vector<FrameStep>& seq) {
-  current_seq_ = &seq;
-  current_seq_index_ = 0;
-  next_byte_ts_ms_ = millis();
-  sequence_active_ = true;
-}
-
-
-void KitchenHood::press_power_on() {
-  request_poweron_ = true;
-}
-
-void KitchenHood::press_power_off() {
-  request_poweroff_ = true;
-}
+void KitchenHood::press_power_on() { request_poweron_ = true; }
+void KitchenHood::press_power_off() { request_poweroff_ = true; }
 
 void KitchenHood::loop() {
-  unsigned long now = millis();
-
-  // Отправка последовательности по одному байту за вызов loop()
-  if (sequence_active_ && current_seq_ && now >= next_byte_ts_ms_) {
-    if (current_seq_index_ < current_seq_->size()) {
-      const auto& step = (*current_seq_)[current_seq_index_];
-      this->write_byte(step.byte);
-      next_byte_ts_ms_ = now + step.pause_bits * bit_duration_ms_;
-      current_seq_index_++;
-    } else {
-      sequence_active_ = false;
-      current_seq_ = nullptr;
-      current_seq_index_ = 0;
-      next_send_ts_ms_ = now + 10 * bit_duration_ms_;
-    }
-  }
-
   switch (state_) {
     case BOOT_SEND:
-      if (!sequence_active_ && boot_repeats_sent_ < boot_repeats_target_) {
-        start_sequence(start_seq_);
+      if (boot_repeats_sent_ < 2) {
+        this->write_array(start_seq_.data(), start_seq_.size());
         boot_repeats_sent_++;
-      } else if (!sequence_active_ && boot_repeats_sent_ >= boot_repeats_target_) {
+      } else {
         state_ = STANDBY;
-        next_send_ts_ms_ = millis();
       }
       break;
 
     case STANDBY:
-      if (!sequence_active_ && now >= next_send_ts_ms_) {
-        if (request_poweron_) {
-          state_ = SENDING_BUTTON;
-          request_poweron_ = false;
-          last_button_action_ = POWER_ON;
-          start_sequence(button_poweron_seq_);
-        } else {
-          start_sequence(standby_seq_);
-        }
-      }
-      break;
-
-    case SENDING_BUTTON:
-      if (!sequence_active_ && now >= next_send_ts_ms_) {
-        if (last_button_action_ == POWER_ON) {
-          state_ = POWERON;
-        } else if (last_button_action_ == POWER_OFF) {
-          state_ = STANDBY;
-        }
-        last_button_action_ = NONE;
-        next_send_ts_ms_ = millis();
+      if (request_poweron_) {
+        this->write_array(button_poweron_seq_.data(), button_poweron_seq_.size());
+        request_poweron_ = false;
+        state_ = POWERON;
+      } else {
+        this->write_array(standby_seq_.data(), standby_seq_.size());
       }
       break;
 
     case POWERON:
-      if (!sequence_active_ && now >= next_send_ts_ms_) {
-        if (request_poweroff_) {
-          state_ = SENDING_BUTTON;
-          request_poweroff_ = false;
-          last_button_action_ = POWER_OFF;
-          start_sequence(button_poweroff_seq_);
-        } else {
-          start_sequence(poweron_seq_);
-        }
+      if (request_poweroff_) {
+        this->write_array(button_poweroff_seq_.data(), button_poweroff_seq_.size());
+        request_poweroff_ = false;
+        state_ = STANDBY;
+      } else {
+        this->write_array(poweron_seq_.data(), poweron_seq_.size());
       }
       break;
   }
--- custom_components/kitchen_hood/kitchen_hood.h
+++ custom_components/kitchen_hood/kitchen_hood.h
@@ -1,65 +1,9 @@
 #pragma once
-
 #include "esphome.h"
 #include "esphome/components/uart/uart.h"
 
 namespace esphome {
 namespace kitchen_hood {
-
-struct FrameStep {
-  uint8_t byte;
-  uint16_t pause_bits; // пауза после этого байта в битах (0 — паузы нет)
-};
-
-class KitchenHood : public Component, public uart::UARTDevice {
- public:
-  explicit KitchenHood(uart::UARTComponent *parent);
-
-  void setup() override;
-  void loop() override;
-
-  void press_power_on();
-  void press_power_off();
-
-  void start_sequence(const std::vector<FrameStep>& seq);
-
-  void set_standby_sequence(const std::vector<FrameStep> &seq) { standby_seq_ = seq; }
-  void set_poweron_sequence(const std::vector<FrameStep> &seq) { poweron_seq_ = seq; }
-  void set_button_poweron_sequence(const std::vector<FrameStep> &seq) { button_poweron_seq_ = seq; }
-  void set_button_poweroff_sequence(const std::vector<FrameStep> &seq) { button_poweroff_seq_ = seq; }
-  void set_bit_duration_ms(uint32_t ms) { bit_duration_ms_ = ms; }
-  void set_start_sequence(const std::vector<FrameStep> &seq) { start_seq_ = seq; }
-
-  static KitchenHood *instance;
-
- protected:
-  enum State { BOOT_SEND, STANDBY, SENDING_BUTTON, POWERON };
-
-  State state_;
-  unsigned long last_ts_ms_;
-  unsigned long next_send_ts_ms_;
-  uint32_t bit_duration_ms_ = 2;
-
-  std::vector<FrameStep> start_seq_;
-  std::vector<FrameStep> standby_seq_;
-  std::vector<FrameStep> poweron_seq_;
-  std::vector<FrameStep> button_poweron_seq_;
-  std::vector<FrameStep> button_poweroff_seq_;
-
-  int boot_repeats_sent_ = 0;
-  int boot_repeats_target_ = 2;
-
-  bool request_poweron_ = false;
-  bool request_poweroff_ = false;
-
-  enum LastButtonAction { NONE, POWER_ON, POWER_OFF };
-  LastButtonAction last_button_action_ = NONE;
-
-  const std::vector<FrameStep>* current_seq_ = nullptr;
-  size_t current_seq_index_ = 0;
-  unsigned long next_byte_ts_ms_ = 0;
-  bool sequence_active_ = false;
-};
 
 class KitchenHoodComponent : public Component {
  public:
@@ -68,6 +12,39 @@ class KitchenHoodComponent : public Component {
 
  protected:
   uart::UARTComponent *uart_;
+};
+
+class KitchenHood : public Component, public uart::UARTDevice {
+ public:
+  explicit KitchenHood(uart::UARTComponent *parent);
+  
+  void setup() override;
+  void loop() override;
+
+  void press_power_on();
+  void press_power_off();
+
+  void set_standby_sequence(const std::vector<uint8_t> &seq) { standby_seq_ = seq; }
+  void set_poweron_sequence(const std::vector<uint8_t> &seq) { poweron_seq_ = seq; }
+  void set_button_poweron_sequence(const std::vector<uint8_t> &seq) { button_poweron_seq_ = seq; }
+  void set_button_poweroff_sequence(const std::vector<uint8_t> &seq) { button_poweroff_seq_ = seq; }
+  void set_start_sequence(const std::vector<uint8_t> &seq) { start_seq_ = seq; }
+
+  static KitchenHood *instance;
+
+ protected:
+  enum State { BOOT_SEND, STANDBY, POWERON };
+  State state_;
+
+  std::vector<uint8_t> start_seq_;
+  std::vector<uint8_t> standby_seq_;
+  std::vector<uint8_t> poweron_seq_;
+  std::vector<uint8_t> button_poweron_seq_;
+  std::vector<uint8_t> button_poweroff_seq_;
+
+  int boot_repeats_sent_ = 0;
+  bool request_poweron_ = false;
+  bool request_poweroff_ = false;
 };
 
 }  // namespace kitchen_hood
