ddc54a2df6ca07b82ded4338c98d1d6815c1fd3b
lagomCat <casiopeia80@gmail.com>
2025-08-10 02:53:30 +0500

    Полная переработка кода.  Максимальная эмуляция оригинальной платы сенсорных кнопок.

--- custom_components/kitchen_hood/kitchen_hood.cpp
+++ custom_components/kitchen_hood/kitchen_hood.cpp
@@ -1,4 +1,6 @@
 #include "kitchen_hood.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 
 namespace esphome {
 namespace kitchen_hood {
@@ -15,11 +17,26 @@ void KitchenHood::setup() {}
 void KitchenHood::press_power_on() { request_poweron_ = true; }
 void KitchenHood::press_power_off() { request_poweroff_ = true; }
 
+
+
+// ====== ОСНОВНОЙ ЦИКЛ ======
 void KitchenHood::loop() {
   switch (state_) {
     case BOOT_SEND:
-      if (boot_repeats_sent_ < 2) {
-        this->write_array(start_seq_.data(), start_seq_.size());
+      if (boot_repeats_sent_ < 1) {
+        kitchen_hood_uart_init();
+        this->write_array(start1_seq_.data(), start1_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
+        this->write_array(start2_seq_.data(), start2_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
+        this->write_array(start3_seq_.data(), start3_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
+        this->write_array(start4_seq_.data(), start4_seq_.size());
+        this->flush();           // Ждём завершения передачи 
+        delay(18);       
         boot_repeats_sent_++;
       } else {
         state_ = STANDBY;
@@ -29,24 +46,39 @@ void KitchenHood::loop() {
     case STANDBY:
       if (request_poweron_) {
         this->write_array(button_poweron_seq_.data(), button_poweron_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
         request_poweron_ = false;
         state_ = POWERON;
       } else {
         this->write_array(standby_seq_.data(), standby_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
       }
       break;
 
     case POWERON:
       if (request_poweroff_) {
         this->write_array(button_poweroff_seq_.data(), button_poweroff_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
         request_poweroff_ = false;
         state_ = STANDBY;
       } else {
         this->write_array(poweron_seq_.data(), poweron_seq_.size());
+        this->flush();           // Ждём завершения передачи
+        delay(18);
       }
       break;
   }
 }
+
+void KitchenHood::kitchen_hood_uart_init() {  
+  const uart_port_t uart_num = UART_NUM_1;
+  uart_set_line_inverse(uart_num, UART_SIGNAL_TXD_INV);
+}
+
+
 
 }  // namespace kitchen_hood
 }  // namespace esphome
--- custom_components/kitchen_hood/kitchen_hood.h
+++ custom_components/kitchen_hood/kitchen_hood.h
@@ -21,6 +21,8 @@ class KitchenHood : public Component, public uart::UARTDevice {
   void setup() override;
   void loop() override;
 
+  void kitchen_hood_uart_init();
+
   void press_power_on();
   void press_power_off();
 
@@ -28,7 +30,10 @@ class KitchenHood : public Component, public uart::UARTDevice {
   void set_poweron_sequence(const std::vector<uint8_t> &seq) { poweron_seq_ = seq; }
   void set_button_poweron_sequence(const std::vector<uint8_t> &seq) { button_poweron_seq_ = seq; }
   void set_button_poweroff_sequence(const std::vector<uint8_t> &seq) { button_poweroff_seq_ = seq; }
-  void set_start_sequence(const std::vector<uint8_t> &seq) { start_seq_ = seq; }
+  void set_start1_sequence(const std::vector<uint8_t> &seq) { start1_seq_ = seq; }
+  void set_start2_sequence(const std::vector<uint8_t> &seq) { start2_seq_ = seq; }
+  void set_start3_sequence(const std::vector<uint8_t> &seq) { start3_seq_ = seq; }
+  void set_start4_sequence(const std::vector<uint8_t> &seq) { start4_seq_ = seq; }
 
   static KitchenHood *instance;
 
@@ -36,7 +41,10 @@ class KitchenHood : public Component, public uart::UARTDevice {
   enum State { BOOT_SEND, STANDBY, POWERON };
   State state_;
 
-  std::vector<uint8_t> start_seq_;
+  std::vector<uint8_t> start1_seq_;
+  std::vector<uint8_t> start2_seq_;
+  std::vector<uint8_t> start3_seq_;
+  std::vector<uint8_t> start4_seq_;
   std::vector<uint8_t> standby_seq_;
   std::vector<uint8_t> poweron_seq_;
   std::vector<uint8_t> button_poweron_seq_;
